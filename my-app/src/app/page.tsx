4ffc82a5a2cddbb166fbb4cb4db0abf2860a43fd
sejinniemma <jsj0471@naver.com>
2025-12-06 23:37:04 +0900

    feat : global.css custom 색상 적용

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <h1>스케쥴러</h1>
+      <h1 className='text-white'>스케쥴러</h1>
     </div>
   );
 }
